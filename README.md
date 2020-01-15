62a01dc7de858e0fee86a0666819de1b91763ee0
Shay Zeevi <shay.z@homepls.co.il>
2020-01-15 11:23:04 +0200

    Added the dev deps for a webpack-typescript project

---
+++ README.md
@@ -0,0 +1,12 @@
+# webpack-starter-typescript
+Install the next packages
+> All in --save-dev
+```json
+"devDependencies": {
+    "ts-loader": "^6.2.1",
+    "typescript": "^3.7.4",
+    "webpack": "^4.41.5",
+    "webpack-cli": "^3.3.10",
+    "webpack-dev-server": "^3.10.1"
+  }
+```
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    performance: { hints: false },
+    mode: 'none',
+    plugins: [
+        new TerserPlugin()
+    ],
+    module: {
+        rules: [
+
+        ]
+    }
+}
