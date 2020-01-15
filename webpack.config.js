64b94fbcb1b8d59b7ee99ced63c4655ebcc79d51
Shay Zeevi <shay.z@homepls.co.il>
2020-01-15 11:43:43 +0200

    webpack and typescript are ready to go

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,20 @@ module.exports = {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
-    performance: { hints: false },
+    performance: {hints: false},
     mode: 'none',
     module: {
         rules: [
-
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: { // Tell webpack to read ts and js files
+        extensions: [
+            '.ts', '.js'
         ]
     }
 }
